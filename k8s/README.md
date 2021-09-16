8916085312377f78a2b9f3f99e5c7967d0dca88c
Mike Miller <chop3593@gmail.com>
2021-09-15 22:36:09 -0600

    feat(k8s): running in k8s

---
+++ k8s/README.md
@@ -0,0 +1,5 @@
+# K8s
+
+## The Lab
+
+In K8s - With a basic ingress for minikube.
--- start_up.sh
+++ start_up.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Turn mongo on
-mongod --fork --syslog
-
 # Start express
 #DEBUG=express:* node bin/www
 node bin/www
